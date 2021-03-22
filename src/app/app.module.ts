3d4287216d4fa14bc607343d07b85ef34286b3fe
carlosrljr13 <c.rl.jr@hotmail.com>
2021-03-22 20:29:31 -0300

    terceiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { RouterModule } from '@angular/router';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     PoModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    PoTemplatesModule
+  ],
+  exports: [LoginComponent]
+})
+export class LoginModule { }
