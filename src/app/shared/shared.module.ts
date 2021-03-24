198f665e126dfd097e441e378b0207c79f421fc2
carlosrljr13 <c.rl.jr@hotmail.com>
2021-03-24 15:48:16 -0300

    commit 2403

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PoModule,
+    HttpClientModule
+  ],
+  exports: [
+    PoModule,
+    HttpClientModule
+  ]
+})
+export class SharedModule { }
